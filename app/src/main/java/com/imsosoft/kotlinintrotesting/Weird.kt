f329a6e02727fcf582bb897bb439c1cb4b160c3c
Muarem <s.muarem10@gmail.com>
2024-02-26 15:00:58 +0400

    created weirdOrNotWeird func

---
+++ app/src/main/java/com/imsosoft/kotlinintrotesting/Weird.kt
@@ -0,0 +1,32 @@
+package com.imsosoft.kotlinintrotesting
+
+class Weird {
+
+    // if N is odd -> Weird
+    // if N is even -> Not Weird
+    // if N is even and in the inclusive range of 2 to 5 -> Not Weird
+    // if N is even and in the inclusive range of 6 to 20 -> Weird
+
+    fun weirdOrNotWeird(n: Int) {
+
+        if (n.isEven()) {
+
+            when (n) {
+                in 2..5 -> {
+                    println("Not Weird")
+                }
+                in 6..20 -> {
+                    println("Weird")
+                }
+                else -> {
+                    println("Not Weird")
+                }
+            }
+
+        } else {
+            println("Weird")
+        }
+
+    }
+
+}
