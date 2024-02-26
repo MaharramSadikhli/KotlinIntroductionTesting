426f45c09452f48d809f20e8e1ee9275058814bc
Muarem <s.muarem10@gmail.com>
2024-02-26 15:32:40 +0400

    Unit weirdOrNotWeird func converted to return func

--- app/src/main/java/com/imsosoft/kotlinintrotesting/Weird.kt
+++ app/src/main/java/com/imsosoft/kotlinintrotesting/Weird.kt
@@ -7,19 +7,23 @@ class Weird {
     // if N is even and in the inclusive range of 2 to 5 -> Not Weird
     // if N is even and in the inclusive range of 6 to 20 -> Weird
 
-    fun weirdOrNotWeird(n: Int) {
+    fun weirdOrNotWeird(n: Int): String {
+
+        var weirdOrNotWeird = ""
 
         if (n.isEven()) {
 
             when (n) {
-                in 2..5 -> println("Not Weird")
-                in 6..20 -> println("Weird")
-                else -> println("Not Weird")
+                in 2..5 -> weirdOrNotWeird = "Not Weird"
+                in 6..20 -> weirdOrNotWeird = "Weird"
+                else -> weirdOrNotWeird = "Not Weird"
             }
 
         } else {
-            println("Weird")
+           weirdOrNotWeird = ""
         }
+
+        return weirdOrNotWeird
 
     }
 
